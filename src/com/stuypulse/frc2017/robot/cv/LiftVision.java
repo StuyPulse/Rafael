a5a92bba1691a7f96bfd0cafda95c056c6740adb
James Wang <jameswang9909@hotmail.com>
2017-01-14 14:01:28 -0500

    Combine broken lift vision targets

--- src/com/stuypulse/frc2017/robot/cv/LiftVision.java
+++ src/com/stuypulse/frc2017/robot/cv/LiftVision.java
@@ -1,6 +1,7 @@
 package com.stuypulse.frc2017.robot.cv;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
@@ -36,18 +37,18 @@ public class LiftVision extends VisionModule {
     public DoubleSliderVariable maxGoalArea = new DoubleSliderVariable("Max Area", 10000.0, 0.0, 10000);
 
     private DeviceCaptureSource liftCamera;
-    
+
     public void initializeCamera() {
     	liftCamera = Camera.initializeCamera(RobotMap.LIFT_CAMERA_PORT);
     }
-    
+
     public void processImage() {
     	if (liftCamera == null) {
     		initializeCamera();
     	}
     	// TODO: Create non-GUI processing method and invoke here
     }
-    
+
     public void run(Mat frame) {
         postImage(frame, "Original");
 
@@ -99,11 +100,11 @@ public class LiftVision extends VisionModule {
         Imgproc.findContours(filtered, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
 
         MatOfPoint2f approxCurve = new MatOfPoint2f();
+        ArrayList<Rect> rects = new ArrayList<Rect>();
 
-        for (int i = 0; i < contours.size(); i++) {
-
+        for (Iterator<MatOfPoint> it = contours.iterator(); it.hasNext();) {
             // boundingRect strategy
-            MatOfPoint2f contour2f = new MatOfPoint2f(contours.get(i).toArray());
+            MatOfPoint2f contour2f = new MatOfPoint2f(it.next().toArray());
             double approxDistance = Imgproc.arcLength(contour2f, true)*0.02;
             Imgproc.approxPolyDP(contour2f, approxCurve, approxDistance, true);
             MatOfPoint points = new MatOfPoint(approxCurve.toArray());
@@ -114,12 +115,34 @@ public class LiftVision extends VisionModule {
             contour2f.release();
 
             double area = rect.area();
-            if (area < minGoalArea.value() || area > maxGoalArea.value()) {
+            if ((area < minGoalArea.value() || area > maxGoalArea.value()) || !aspectRatioThreshold(rect.width, rect.height)) {
+                it.remove();
                 continue;
             }
-            if (!aspectRatioThreshold(rect.width, rect.height)) {
-                continue;
+            int i = 0;
+            while (i < rects.size() && rects.get(i).area() < area) {
+                i++;
             }
+            rects.add(i, rect);
+        }
+
+        // One vision target was broken up by the peg
+        if (rects.size() == 3) {
+            Rect r1 = rects.get(0);
+            Rect r2 = rects.get(1);
+            MatOfPoint p = new MatOfPoint(
+                    new Point(r1.x, r1.y),
+                    new Point(r1.x+r1.width, r1.y+r1.height),
+                    new Point(r2.x, r2.y),
+                    new Point(r2.x+r2.width, r2.y+r2.height));
+            Rect combined = Imgproc.boundingRect(p);
+            rects.remove(0);
+            rects.remove(0);
+            rects.add(combined);
+        }
+
+        for (int i = 0; i < rects.size(); i++) {
+            Rect rect = rects.get(i);
             Imgproc.rectangle(drawn, new Point(rect.x,rect.y), new Point(rect.x+rect.width,rect.y+rect.height), new Scalar(0, 255, 0), 1);
 
             // RotatedRect strategy
