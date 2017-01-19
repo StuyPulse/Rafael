4e2ecea34c494fd1a2ac87a48415c7a0e2af8c0c
James Wang <jameswang9909@hotmail.com>
2017-01-18 20:04:58 -0500

    Add method for just configuring a camera

--- src/com/stuypulse/frc2017/robot/Robot.java
+++ src/com/stuypulse/frc2017/robot/Robot.java
@@ -27,92 +27,91 @@ import com.stuypulse.frc2017.robot.subsystems.ExampleSubsystem;
  */
 public class Robot extends IterativeRobot {
 
-	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
-	public static OI oi;
+    public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
+    public static OI oi;
 
-	Command autonomousCommand;
-	SendableChooser<Command> chooser = new SendableChooser<>();
+    Command autonomousCommand;
+    SendableChooser<Command> chooser = new SendableChooser<>();
 
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
-	@Override
-	public void robotInit() {
-		oi = new OI();
-		chooser.addDefault("Default Auto", new ExampleCommand());
-		// chooser.addObject("My Auto", new MyAutoCommand());
-		SmartDashboard.putData("Auto mode", chooser);
-	}
+    UsbCamera boilerCamera;
+    UsbCamera liftCamera;
 
-	/**
-	 * This function is called once each time the robot enters Disabled mode.
-	 * You can use it to reset any subsystem information you want to clear when
-	 * the robot is disabled.
-	 */
-	@Override
-	public void disabledInit() {
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    @Override
+    public void robotInit() {
+        oi = new OI();
+        chooser.addDefault("Default Auto", new ExampleCommand());
+        // chooser.addObject("My Auto", new MyAutoCommand());
+        SmartDashboard.putData("Auto mode", chooser);
 
-	}
+        boilerCamera = new UsbCamera("Boiler Camera", 0);
+        liftCamera = new UsbCamera("Lift Camera", 1);
+    }
 
-	@Override
-	public void disabledPeriodic() {
-		Scheduler.getInstance().run();
-	}
+    /**
+     * This function is called once each time the robot enters Disabled mode.
+     * You can use it to reset any subsystem information you want to clear when
+     * the robot is disabled.
+     */
+    @Override
+    public void disabledInit() {
 
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString code to get the auto name from the text box below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional commands to the
-	 * chooser code above (like the commented example) or additional comparisons
-	 * to the switch structure below with additional strings & commands.
-	 */
-	@Override
-	public void autonomousInit() {
-		autonomousCommand = chooser.getSelected();
+    }
 
-		/*
-		 * String autoSelected = SmartDashboard.getString("Auto Selector",
-		 * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
-		 * = new MyAutoCommand(); break; case "Default Auto": default:
-		 * autonomousCommand = new ExampleCommand(); break; }
-		 */
+    @Override
+    public void disabledPeriodic() {
+        Scheduler.getInstance().run();
+    }
 
-		// schedule the autonomous command (example)
-		if (autonomousCommand != null)
-			autonomousCommand.start();
-	}
+    /**
+     * This autonomous (along with the chooser code above) shows how to select
+     * between different autonomous modes using the dashboard. The sendable
+     * chooser code works with the Java SmartDashboard. If you prefer the
+     * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+     * getString code to get the auto name from the text box below the Gyro
+     *
+     * You can add additional auto modes by adding additional commands to the
+     * chooser code above (like the commented example) or additional comparisons
+     * to the switch structure below with additional strings & commands.
+     */
+    @Override
+    public void autonomousInit() {
+        autonomousCommand = chooser.getSelected();
 
-	/**
-	 * This function is called periodically during autonomous
-	 */
-	@Override
-	public void autonomousPeriodic() {
-		Scheduler.getInstance().run();
-	}
+        /*
+         * String autoSelected = SmartDashboard.getString("Auto Selector",
+         * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
+         * = new MyAutoCommand(); break; case "Default Auto": default:
+         * autonomousCommand = new ExampleCommand(); break; }
+         */
 
-	@Override
-	public void teleopInit() {
-		// This makes sure that the autonomous stops running when
-		// teleop starts running. If you want the autonomous to
-		// continue until interrupted by another command, remove
-		// this line or comment it out.
-		if (autonomousCommand != null)
-			autonomousCommand.cancel();
+        // schedule the autonomous command (example)
+        if (autonomousCommand != null)
+            autonomousCommand.start();
+    }
 
-		System.out.println("In teleopInit()");
-        UsbCamera boilerCamera = new UsbCamera("Boiler Camera", 2);
-        UsbCamera liftCamera = new UsbCamera("Lift Camera", 3);
+    /**
+     * This function is called periodically during autonomous
+     */
+    @Override
+    public void autonomousPeriodic() {
+        Scheduler.getInstance().run();
+    }
 
-        System.out.println("Initialized cameras");
+    @Override
+    public void teleopInit() {
+        // This makes sure that the autonomous stops running when
+        // teleop starts running. If you want the autonomous to
+        // continue until interrupted by another command, remove
+        // this line or comment it out.
+        if (autonomousCommand != null)
+            autonomousCommand.cancel();
 
         boilerCamera.setResolution(160, 120);
         liftCamera.setResolution(160, 120);
-
         System.out.println("Set resolutions");
 
         CvSink boilerSink = new CvSink("Boiler Camera Sink");
@@ -125,6 +124,9 @@ public class Robot extends IterativeRobot {
         Mat boilerFrame = new Mat();
         Mat liftFrame = new Mat();
 
+        Camera.configureCamera(0);
+        Camera.configureCamera(1);
+
         boilerSink.grabFrame(boilerFrame);
         liftSink.grabFrame(liftFrame);
         System.out.println("Read frames");
@@ -132,21 +134,21 @@ public class Robot extends IterativeRobot {
         Imgcodecs.imwrite("/tmp/boiler.png", boilerFrame);
         Imgcodecs.imwrite("/tmp/lift.png", liftFrame);
         System.out.println("Wrote images");
-	}
+    }
 
-	/**
-	 * This function is called periodically during operator control
-	 */
-	@Override
-	public void teleopPeriodic() {
-		Scheduler.getInstance().run();
-	}
+    /**
+     * This function is called periodically during operator control
+     */
+    @Override
+    public void teleopPeriodic() {
+        Scheduler.getInstance().run();
+    }
 
-	/**
-	 * This function is called periodically during test mode
-	 */
-	@Override
-	public void testPeriodic() {
-		LiveWindow.run();
-	}
+    /**
+     * This function is called periodically during test mode
+     */
+    @Override
+    public void testPeriodic() {
+        LiveWindow.run();
+    }
 }
--- src/com/stuypulse/frc2017/robot/cv/Camera.java
+++ src/com/stuypulse/frc2017/robot/cv/Camera.java
@@ -8,22 +8,28 @@ import stuyvision.VisionModule;
 import stuyvision.capture.DeviceCaptureSource;
 
 public class Camera {
-	
-	private static final String pathToV4L2 = "/usr/bin/v4l2-ctl";
-	
-    public static DeviceCaptureSource initializeCamera(int cameraPort) {
+
+    private static final String pathToV4L2 = "/usr/bin/v4l2-ctl";
+
+    public static boolean configureCamera(int port) {
         Runtime rt = Runtime.getRuntime();
-        String cmdStart = pathToV4L2 + " -d " + cameraPort + " ";
+        String cmdStart = pathToV4L2 + " -d " + port + " ";
         try {
             rt.exec(cmdStart + "-c exposure_auto=1,exposure_absolute=5,brightness=30,contrast=10,saturation=200,white_balance_temperature_auto=0,sharpness=50").waitFor();
             rt.exec(cmdStart + "-c white_balance_temperature=4624").waitFor();
+            System.out.println("Finished configuring");
+            return true;
         } catch (Exception e) {
             System.err.println("Setting v4l settings crashed!");
+            return false;
         }
+    }
+
+    public static DeviceCaptureSource initializeCamera(int cameraPort) {
         DeviceCaptureSource camera = new DeviceCaptureSource(cameraPort);
         System.out.println("Made camera at " + cameraPort);
+        configureCamera(cameraPort);
         camera.setBufferSize(2);
         return camera;
     }
-
 }
