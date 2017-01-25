5374f7493955afa3c4a6abb5575b838f62b518a1
Helen Ye <00yhelen@gmail.com>
2017-01-25 18:01:51 -0500

    Create interface for CV constants

---
+++ src/com/stuypulse/frc2017/robot/CVConstants.java
@@ -0,0 +1,11 @@
+package com.stuypulse.frc2017.robot;
+
+public interface CVConstants {
+
+    // TODO
+    // Tune threshold for the possible offset of the y coordinate of the target
+    public static double LIFT_HEIGHT_X_THRESHOLD = 0.5;
+
+    public static double LIFT_TARGET_HEIGHT = 15.75;
+
+}
