0eacb61cb52dbfbc0856b278942b04c75bf65c8e
No Wangey <prog694@gmail.com>
2017-01-25 20:37:12 -0500

    modify blender and fized import error in drivetrain

--- src/com/stuypulse/frc2017/robot/subsystems/Blender.java
+++ src/com/stuypulse/frc2017/robot/subsystems/Blender.java
@@ -1,11 +1,21 @@
 package com.stuypulse.frc2017.robot.subsystems;
 
+import com.stuypulse.frc2017.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.command.Subsystem;
+
 
 /**
  *
  */
 public class Blender extends Subsystem {
+    public CANTalon blenderMotor;
+    
+    public Blender() {
+        blenderMotor = new CANTalon(RobotMap.BLENDER_MOTOR_PORT);
+    }
+   
 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
