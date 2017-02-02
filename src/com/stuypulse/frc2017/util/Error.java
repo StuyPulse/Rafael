d606b0538c9b220c7e6e93a48d85f6de9e44c6e3
Robin Han <robin.han.stuy@gmail.com>
2017-02-02 17:03:32 -0500

    Fix variable modifiers in Error.java

--- src/com/stuypulse/frc2017/util/Error.java
+++ src/com/stuypulse/frc2017/util/Error.java
@@ -13,29 +13,29 @@ public class Error {
 		hypo3[1] = 0;
 	}
 	public void update(double angle){
-		private double angle1 = math.abs(hypo1[0]-angle);
-		private double angle2 = math.abs(hypo2[0]-angle);
-		private double angle3 = math.abs(hypo3[0]-angle);
+		double angle1 = Math.abs(hypo1[0]-angle);
+		double angle2 = Math.abs(hypo2[0]-angle);
+		double angle3 = Math.abs(hypo3[0]-angle);
 		if ((angle1 > angle2) && (angle1 > angle3)){
-			private double anglefin = hypo1[0]-angle;
+			double anglefin = hypo1[0]-angle;
 			hypo1[0] -= anglefin / hypo1[1];
 			hypo1[1] += 10;
 		}
 		if ((angle2 > angle1) && (angle2 > angle3)){
-			private double anglefin = hypo2[0]-angle;
+			double anglefin = hypo2[0]-angle;
 			hypo2[0] -= anglefin / hypo2[1];
 			hypo2[1] += 10;
 		}
 		if ((angle3 > angle2) && (angle3 > angle1)){
-			private double anglefin = hypo3[0]-angle;
+			double anglefin = hypo3[0]-angle;
 			hypo3[0] -= anglefin / hypo3[1];
 			hypo3[1] += 10;
 		}
 	}
 	public double angler(){
-		private double average = hypo1[0] * hypo1[1] +hypo2[0] * hypo2[1]
+		double average = hypo1[0] * hypo1[1] +hypo2[0] * hypo2[1]
 				+hypo3[0] * hypo3[1];
-		private double totaltries = hypo1[1]+hypo2[1]+hypo3[1];
+		double totaltries = hypo1[1]+hypo2[1]+hypo3[1];
 		return average/totaltries;
 	}
 }
