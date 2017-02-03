ff167361e01fb1f8f29adb08eab184ba816c5c34
adrisj7 <adrisj7@gmail.com>
2017-02-03 18:25:50 -0500

    Formatting

--- src/com/stuypulse/frc2017/robot/commands/auton/TurnCommand.java
+++ src/com/stuypulse/frc2017/robot/commands/auton/TurnCommand.java
@@ -11,7 +11,7 @@ public class TurnCommand extends Command {
     private double time;
     private double angle;
     private double speed;
-    
+
     public TurnCommand(double angle, double time, double speed) {
         // Use requires() here to declare subsystem dependencies
         requires(Robot.drivetrain);
@@ -24,23 +24,20 @@ public class TurnCommand extends Command {
     protected void initialize() {
         Robot.drivetrain.resetGyro();
         setTimeout(time);
-        if(angle > 0){
-            Robot.drivetrain.tankDrive(speed,-speed);
-        
-        
-        }else{
-            Robot.drivetrain.tankDrive(-speed,speed);
+        if (angle > 0) {
+            Robot.drivetrain.tankDrive(speed, -speed);
+        } else {
+            Robot.drivetrain.tankDrive(-speed, speed);
         }
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-         return  Math.abs(Robot.drivetrain.gyroAngle()) >= Math.abs(angle) || isTimedOut();
+        return Math.abs(Robot.drivetrain.gyroAngle()) >= Math.abs(angle) || isTimedOut();
     }
 
     // Called once after isFinished returns true
