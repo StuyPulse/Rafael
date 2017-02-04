d3d10c7fc207984d9a154acf1c6fa8111e00b351
James Wang <jameswang9909@hotmail.com>
2017-02-04 10:50:02 -0500

    Add commands for dynamically rotating and driving

---
+++ src/com/stuypulse/frc2017/robot/commands/DriveForwardEncodersCommand.java
@@ -0,0 +1,33 @@
+package com.stuypulse.frc2017.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ *
+ */
+public class DriveForwardEncodersCommand extends EncoderDrivingCommand {
+
+    private double inches;
+    private boolean set;
+
+    public DriveForwardEncodersCommand() {
+        super();
+        set = false;
+    }
+
+    public DriveForwardEncodersCommand(double inches) {
+        super();
+        this.inches = inches;
+        set = true;
+    }
+
+    protected void setInchesToMove(double inches) {
+        this.inches = inches;
+    }
+
+    @Override
+    protected void setInchesToMove() {
+        initialInchesToMove = set ? inches : SmartDashboard.getNumber("encoder-drive-inches");
+    }
+}
