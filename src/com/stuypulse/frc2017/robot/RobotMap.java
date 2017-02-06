0b94b7161daf03edb4a796280e0ac8cd47a84d0a
blai00 <blai00@stuy.edu>
2017-02-06 18:14:34 -0500

    Add constants to RobotMap and write command for BoilerGear.

--- src/com/stuypulse/frc2017/robot/RobotMap.java
+++ src/com/stuypulse/frc2017/robot/RobotMap.java
@@ -62,7 +62,7 @@ public interface RobotMap {
 	int PCM_4 = -1;
 	int GEAR_SHIFT = -1;
 
-    // Robot Physical Constants
+    // Physical Constants
 
     double DRIVETRAIN_ENCODER_INCHES_PER_PULSE = 23.56;
 
@@ -77,9 +77,9 @@ public interface RobotMap {
     //TODO: Find Ideal Shooter Speed
     double SHOOTER_IDEAL_SPEED = -1;
     
-    // Field Physical Constants
-
-    double START_TO_MIDDLE_GEAR_DISTANCE = -1;//9.5 * 12.0;
-    double MIDDLE_REVERSE_DISTANCE = -1;
-
+    //Field Constants?
+    double START_TO_BOILER_GEAR_DISTANCE = -1;
+    double START_TO_BOILER_GEAR_ANGLE = -1;
+    double AFTER_TURN_TO_BOILER_GEAR_DISTANCE = -1;
+    double BOILER_GEAR_TO_TURN_DISTANCE = -1;
 }
---
+++ src/com/stuypulse/frc2017/robot/commands/auton/ScoreBoilerGearCommand.java
@@ -0,0 +1,42 @@
+package com.stuypulse.frc2017.robot.commands.auton;
+
+import com.stuypulse.frc2017.robot.RobotMap;
+import com.stuypulse.frc2017.robot.commands.DriveForwardEncodersCommand;
+import com.stuypulse.frc2017.robot.commands.GearPusherRetractGearCommand;
+import com.stuypulse.frc2017.robot.commands.GearTrapReleaseGearCommand;
+import com.stuypulse.frc2017.robot.commands.GearTrapTrapGearCommand;
+import com.stuypulse.frc2017.robot.commands.RotateDegreesGyroCommand;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class ScoreBoilerGearCommand extends CommandGroup {
+    
+    public  ScoreBoilerGearCommand() {
+        // Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
+
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
+
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
+    	addSequential(new DriveForwardEncodersCommand(RobotMap.START_TO_BOILER_GEAR_DISTANCE));
+    	addSequential(new RotateDegreesGyroCommand(RobotMap.START_TO_BOILER_GEAR_ANGLE));
+    	addSequential(new DriveForwardEncodersCommand(RobotMap.AFTER_TURN_TO_BOILER_GEAR_DISTANCE));
+    	addSequential(new GearTrapReleaseGearCommand());
+    	addSequential(new GearPusherRetractGearCommand());
+    	addSequential(new GearTrapTrapGearCommand());
+    	addSequential(new DriveForwardEncodersCommand(RobotMap.BOILER_GEAR_TO_TURN_DISTANCE));
+    }
+}
