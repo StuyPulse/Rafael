5ae2a12b4b610c2bf0918897579c60196f667d08
blai00 <blai00@stuy.edu>
2017-02-07 17:22:03 -0500

    Add Red/Blue Alliance option for auton.

--- src/com/stuypulse/frc2017/robot/commands/auton/ScoreBoilerGearCommand.java
+++ src/com/stuypulse/frc2017/robot/commands/auton/ScoreBoilerGearCommand.java
@@ -14,7 +14,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  */
 public class ScoreBoilerGearCommand extends CommandGroup {
     
-    public  ScoreBoilerGearCommand() {
+    public  ScoreBoilerGearCommand(boolean isRedAlliance) {
         // Add Commands here:
         // e.g. addSequential(new Command1());
         //      addSequential(new Command2());
@@ -31,12 +31,25 @@ public class ScoreBoilerGearCommand extends CommandGroup {
         // e.g. if Command1 requires 	chassis, and Command2 requires arm,
         // a CommandGroup containing them would require both the chassis and the
         // arm.
-    	addSequential(new DriveForwardEncodersCommand(RobotMap.START_TO_BOILER_GEAR_TURN_DISTANCE));
-    	addSequential(new RotateDegreesGyroCommand(RobotMap.BOILER_GEAR_TURN_TO_BOILER_GEAR_ANGLE));
-    	addSequential(new DriveForwardEncodersCommand(RobotMap.AFTER_TURN_TO_BOILER_GEAR_DISTANCE));
-    	addSequential(new GearTrapReleaseGearCommand());
-    	addSequential(new GearPusherRetractGearCommand());
-    	addSequential(new DriveForwardEncodersCommand(RobotMap.BOILER_GEAR_REVERSE_DISTANCE));
-    	addSequential(new GearTrapTrapGearCommand());
+    	
+    	if (isRedAlliance){
+    		addSequential(new DriveForwardEncodersCommand(RobotMap.START_TO_BOILER_GEAR_TURN_DISTANCE));
+    		addSequential(new RotateDegreesGyroCommand(RobotMap.BOILER_GEAR_TURN_TO_BOILER_GEAR_ANGLE));
+    		addSequential(new DriveForwardEncodersCommand(RobotMap.AFTER_TURN_TO_BOILER_GEAR_DISTANCE));
+    		addSequential(new GearTrapReleaseGearCommand());
+    		addSequential(new GearPusherRetractGearCommand());
+    		addSequential(new DriveForwardEncodersCommand(RobotMap.BOILER_GEAR_REVERSE_DISTANCE));
+    		addSequential(new GearTrapTrapGearCommand());
+    	}
+    	
+    	else{
+    		addSequential(new DriveForwardEncodersCommand(RobotMap.START_TO_BOILER_GEAR_TURN_DISTANCE));
+    		addSequential(new RotateDegreesGyroCommand(-1 * RobotMap.BOILER_GEAR_TURN_TO_BOILER_GEAR_ANGLE));
+    		addSequential(new DriveForwardEncodersCommand(RobotMap.AFTER_TURN_TO_BOILER_GEAR_DISTANCE));
+    		addSequential(new GearTrapReleaseGearCommand());
+    		addSequential(new GearPusherRetractGearCommand());
+    		addSequential(new DriveForwardEncodersCommand(RobotMap.BOILER_GEAR_REVERSE_DISTANCE));
+    		addSequential(new GearTrapTrapGearCommand());
+    	}
     }
 }
