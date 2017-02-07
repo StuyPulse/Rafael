29f6af9b58585aed77bf3a1cd0a10e806e914b26
Brian Lin <blin5@stuy.edu>
2017-02-06 20:11:04 -0500

    Add ApproachHPFromMiddleGearCommand to auton

---
+++ src/com/stuypulse/frc2017/robot/commands/ApproachHPFromMiddleGearCommand.java
@@ -0,0 +1,37 @@
+package com.stuypulse.frc2017.robot.commands;
+
+import com.stuypulse.frc2017.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class ApproachHPFromMiddleGearCommand extends CommandGroup {
+
+    public ApproachHPFromMiddleGearCommand() {
+        // Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
+
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
+
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
+        addSequential(new RotateDegreesGyroCommand(RobotMap.MIDDLE_GEAR_TO_AIRSHIP_BOILER_SIDE_ANGLE));
+        addSequential(new DriveForwardEncodersCommand(RobotMap.MIDDLE_GEAR_TO_AIRSHIP_BOILER_SIDE_DISTANCE));
+        addSequential(new RotateDegreesGyroCommand(RobotMap.AIRSHIP_BOILER_SIDE_TO_NEUTRAL_ZONE_ANGLE));
+        addSequential(new DriveForwardEncodersCommand(RobotMap.AIRSHIP_BOILER_SIDE_TO_NEUTRAL_ZONE_DISTANCE));
+        addSequential(new RotateDegreesGyroCommand(RobotMap.NEUTRAL_ZONE_BOILER_SIDE_TO_HP_ANGLE));
+        addSequential(new DriveForwardEncodersCommand(RobotMap.NEUTRAL_ZONE_BOILER_SIDE_TO_HP_DISTANCE));
+        
+    }
+}
