807d2f913f9bdbaccbbb000ad16067ab31f50046
Hui Min Wu <huiminthehuman@huimin.wu-UX305CA>
2017-02-07 20:13:20 -0500

    Make code for ApproachHPFromMiddleGearCommand more concise

--- src/com/stuypulse/frc2017/robot/commands/auton/ApproachHPFromMiddleGearCommand.java
+++ src/com/stuypulse/frc2017/robot/commands/auton/ApproachHPFromMiddleGearCommand.java
@@ -12,32 +12,16 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class ApproachHPFromMiddleGearCommand extends CommandGroup {
 
 	public ApproachHPFromMiddleGearCommand(boolean isRedAlliance) {
-		// Add Commands here:
-		// e.g. addSequential(new Command1());
-		// addSequential(new Command2());
-		// these will run in order.
-
-		// To run multiple commands at the same time,
-		// use addParallel()
-		// e.g. addParallel(new Command1());
-		// addSequential(new Command2());
-		// Command1 and Command2 will run in parallel.
-
-		// A command group will require all of the subsystems that each member
-		// would require.
-		// e.g. if Command1 requires chassis, and Command2 requires arm,
-		// a CommandGroup containing them would require both the chassis and the
-		// arm.
+		int direction;
 		if (isRedAlliance) {
-			addSequential(new RotateDegreesGyroCommand(RobotMap.MIDDLE_GEAR_TO_AIRSHIP_HP_SIDE_ANGLE));
-			addSequential(new DriveForwardEncodersCommand(RobotMap.MIDDLE_GEAR_TO_AIRSHIP_HP_SIDE_DISTANCE));
-			addSequential(new RotateDegreesGyroCommand(RobotMap.AIRSHIP_HP_SIDE_TO_NEUTRAL_ZONE_ANGLE));
-			addSequential(new DriveForwardEncodersCommand(RobotMap.AIRSHIP_HP_SIDE_TO_NEUTRAL_ZONE_DISTANCE));
+			direction = 1;
 		} else {
-			addSequential(new RotateDegreesGyroCommand(-1 * RobotMap.MIDDLE_GEAR_TO_AIRSHIP_HP_SIDE_ANGLE));
-			addSequential(new DriveForwardEncodersCommand(RobotMap.MIDDLE_GEAR_TO_AIRSHIP_HP_SIDE_DISTANCE));
-			addSequential(new RotateDegreesGyroCommand(-1 * RobotMap.AIRSHIP_HP_SIDE_TO_NEUTRAL_ZONE_ANGLE));
-			addSequential(new DriveForwardEncodersCommand(RobotMap.AIRSHIP_HP_SIDE_TO_NEUTRAL_ZONE_DISTANCE));
+			direction = -1;
 		}
+		
+		addSequential(new RotateDegreesGyroCommand(RobotMap.MIDDLE_GEAR_TO_AIRSHIP_HP_SIDE_ANGLE * direction));
+		addSequential(new DriveForwardEncodersCommand(RobotMap.MIDDLE_GEAR_TO_AIRSHIP_HP_SIDE_DISTANCE));
+		addSequential(new RotateDegreesGyroCommand(RobotMap.AIRSHIP_HP_SIDE_TO_NEUTRAL_ZONE_ANGLE * direction));
+		addSequential(new DriveForwardEncodersCommand(RobotMap.AIRSHIP_HP_SIDE_TO_NEUTRAL_ZONE_DISTANCE));
 	}
 }
