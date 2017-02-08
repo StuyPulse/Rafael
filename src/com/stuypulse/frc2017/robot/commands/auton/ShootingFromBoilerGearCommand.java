8db2738b7893c8fe412a227d68090a4337dddeb6
Brian Lin <blin5@stuy.edu>
2017-02-08 17:29:47 -0500

    Add an auton command for shooting from boiler gear

---
+++ src/com/stuypulse/frc2017/robot/commands/auton/ShootingFromBoilerGearCommand.java
@@ -0,0 +1,33 @@
+package com.stuypulse.frc2017.robot.commands.auton;
+
+import com.stuypulse.frc2017.robot.RobotMap;
+import com.stuypulse.frc2017.robot.commands.BlenderRunWithUnjammingCommand;
+import com.stuypulse.frc2017.robot.commands.DriveForwardEncodersCommand;
+import com.stuypulse.frc2017.robot.commands.RotateDegreesGyroCommand;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class ShootingFromBoilerGearCommand extends CommandGroup {
+
+    public ShootingFromBoilerGearCommand(boolean isRedAlliance) {
+        
+        addSequential (new ScoreBoilerGearCommand(isRedAlliance));
+        
+        int direction;
+        if (isRedAlliance) {
+            direction = 1;
+        } else {
+            direction = -1;
+       
+        addSequential(new DriveForwardEncodersCommand(RobotMap.BOILER_GEAR_REVERSE_SHOOTING_DISTANCE));
+        addSequential(new RotateDegreesGyroCommand(RobotMap.BOILER_GEAR_TURN_TO_BOILER_ANGLE * direction));
+        addSequential(new DriveForwardEncodersCommand(RobotMap.BOILER_GEAR_TO_BOILER_DISTANCE));
+        //TODO: CV align w boiler here
+        addSequential(new BlenderRunWithUnjammingCommand());
+        
+    }
+}
+}
