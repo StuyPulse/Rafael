e89090e1779f14daf6e029aa9f795d45cc40e8b0
BRIAN LIN <BRIAN LIN>
2017-02-08 17:39:07 -0500

    Fix logic in gearCheckTime method

--- src/com/stuypulse/frc2017/util/IRSensor.java
+++ src/com/stuypulse/frc2017/util/IRSensor.java
@@ -1,11 +1,11 @@
 package com.stuypulse.frc2017.util;
 
-import java.util.Timer;
 
 import com.stuypulse.frc2017.robot.Robot;
 import com.stuypulse.frc2017.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.Timer;
 
 
 /**
@@ -15,16 +15,13 @@ public class IRSensor {
     
     private static final double TRIAL_AND_ERROR_NUMBER = 12.23368994;	
 
-	private static final double EQUATION_EXPONENT = -.9779601588;
+	private static final double EQUATION_EXPONENT = -0.9779601588;
 
 	private static final double CONVERSION_FACTOR_CM_TO_INCHES = 0.393701;
 
 	// Put methods for controlling this subsystem
     // here. Call these from Commands.
     private static AnalogInput distanceSensor;
-    
-    // Keeps track of whether the gear was previously detected
-    private static boolean gearWasDetected;
 
     // Create instance of a timer that we can use to keep track of how long the 
     // gear is kept in the position for. 
@@ -32,7 +29,6 @@ public class IRSensor {
 
     public IRSensor() {
         distanceSensor = new AnalogInput(RobotMap.IR_SENSOR_PORT);
-        gearWasDetected = false;
         timeSinceEntry = new Timer();
         
     }
@@ -53,18 +49,14 @@ public class IRSensor {
     // TODO: Look over logic
     public static void gearCheckTime() {
        if (gearInMechanism()){
-    	   if (gearWasDetected) {
+           timeSinceEntry.start();
                if (timeSinceEntry.get() > RobotMap.IR_TIME_IN_MECHANISM_THRESHOLD) {
                    Robot.gearpusher.extend();
                    timeSinceEntry.stop();
                    timeSinceEntry.reset();
-                   gearWasDetected = false;
+                   Robot.gearpusher.retract();
                }
-           }else {
-        	   timeSinceEntry.start();
-        	   gearWasDetected = true;
            }
-       }
    }
  }
 
