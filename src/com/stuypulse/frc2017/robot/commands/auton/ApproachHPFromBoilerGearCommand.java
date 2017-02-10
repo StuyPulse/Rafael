e0358a1be2cb7b902d9d866c24d7dc608ed7588b
Hui Min Wu <huiminthehuman@huimin.wu-UX305CA>
2017-02-10 17:25:44 -0500

    Add ability to go to high gear when approaching human player

--- src/com/stuypulse/frc2017/robot/commands/auton/ApproachHPFromBoilerGearCommand.java
+++ src/com/stuypulse/frc2017/robot/commands/auton/ApproachHPFromBoilerGearCommand.java
@@ -2,6 +2,7 @@ package com.stuypulse.frc2017.robot.commands.auton;
 
 import com.stuypulse.frc2017.robot.RobotMap;
 import com.stuypulse.frc2017.robot.commands.DriveForwardEncodersCommand;
+import com.stuypulse.frc2017.robot.commands.DriveTrainHighGearCommand;
 import com.stuypulse.frc2017.robot.commands.RotateDegreesGyroCommand;
 
 import edu.wpi.first.wpilibj.DriverStation;
@@ -19,10 +20,10 @@ public class ApproachHPFromBoilerGearCommand extends CommandGroup {
 		} else {
 			direction = -1;
 		}
+		addSequential(new DriveTrainHighGearCommand());
         addSequential(new RotateDegreesGyroCommand((direction * RobotMap.BOILER_GEAR_TO_NEUTRAL_ZONE_ANGLE)));
     	addSequential(new DriveForwardEncodersCommand(RobotMap.BOILER_GEAR_TO_NEUTRAL_ZONE_DISTANCE));
     	addSequential(new RotateDegreesGyroCommand(direction * RobotMap.BOILER_GEAR_NEUTRAL_ZONE_TO_HP_ANGLE));
     	addSequential(new DriveForwardEncodersCommand(RobotMap.BOILER_GEAR_NEUTRAL_ZONE_TO_HP_DISTANCE));
 	}
 }
-
