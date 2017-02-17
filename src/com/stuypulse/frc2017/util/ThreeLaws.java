419a22bd9b35f22972b0b408b421b115d0e45bbb
Timothy So <so.timothy1@gmail.com>
2017-02-17 18:25:50 -0500

    Asimov's three laws.

---
+++ src/com/stuypulse/frc2017/util/ThreeLaws.java
@@ -0,0 +1,27 @@
+package com.stuypulse.frc2017.util;
+
+public class ThreeLaws {
+
+	
+	
+	public void FirstLaw() {
+		/*
+		 * 
+		 */
+	}
+	
+	public void SecondLaw() {
+		/*
+		 * 
+		 */
+		
+	}
+	
+	public void ThirdLaw() {
+		
+		/*
+		 * 
+		 */
+		
+	}
+}
