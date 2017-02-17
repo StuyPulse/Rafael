53fb59bc1c0b76baf0a0d403e74427fc3a2e21ca
Wilson Berkow <wilsonberkow@gmail.com>
2017-02-16 19:09:39 -0500

    Comment and autoformat OptionalCVPosition* commands

--- src/com/stuypulse/frc2017/robot/commands/auton/OptionalCVPositionForGearCommand.java
+++ src/com/stuypulse/frc2017/robot/commands/auton/OptionalCVPositionForGearCommand.java
@@ -7,65 +7,67 @@ import com.stuypulse.frc2017.robot.commands.cv.SetupForGearCommand;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
- *
+ * Used in autonomous to either drive a set number of inches to the boiler, if
+ * the auton CV chooser on SmartDashboard was selected to not use CV in auton,
+ * or to drive to the boiler with CV alignment otherwise.
  */
 public class OptionalCVPositionForGearCommand extends Command {
-	// instantiate both commands in constructor in case command framework requires it
-	private SetupForGearCommand cvCommand;
-	private DriveForwardEncodersCommand driveCommand;
+    // instantiate both commands in constructor in case command framework
+    // requires it
+    private SetupForGearCommand cvCommand;
+    private DriveForwardEncodersCommand driveCommand;
 
-	public OptionalCVPositionForGearCommand(double approxDistance) {
-		requires(Robot.drivetrain);
-		cvCommand = new SetupForGearCommand();
-		driveCommand = new DriveForwardEncodersCommand(approxDistance);
-	}
+    public OptionalCVPositionForGearCommand(double approxDistance) {
+        requires(Robot.drivetrain);
+        cvCommand = new SetupForGearCommand();
+        driveCommand = new DriveForwardEncodersCommand(approxDistance);
+    }
 
-	// Called just before this Command runs the first time
-	protected void initialize() {
-		if (Robot.cvChooser.getSelected()) {
-			cvCommand.initialize();
+    // Called just before this Command runs the first time
+    protected void initialize() {
+        if (Robot.cvChooser.getSelected()) {
+            cvCommand.initialize();
 
-		} else {
-			driveCommand.initialize();
+        } else {
+            driveCommand.initialize();
 
-		}
-	}
+        }
+    }
 
+    // Called repeatedly when this Command is scheduled to rune
+    protected void execute() {
+        if (Robot.cvChooser.getSelected()) {
+            cvCommand.execute();
+        } else {
+            driveCommand.execute();
+        }
+    }
 
-	// Called repeatedly when this Command is scheduled to rune
-	protected void execute() {
-	    if (Robot.cvChooser.getSelected()){
-	        cvCommand.execute();
-	    }else{
-	        driveCommand.execute();
-	    }
-	}
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        if (Robot.cvChooser.getSelected()) {
+            return cvCommand.isFinished();
+        } else {
+            return driveCommand.isFinished();
+        }
+    }
 
-	// Make this return true when this Command no longer needs to run execute()
-	protected boolean isFinished() {
-		if (Robot.cvChooser.getSelected()){
-		        return cvCommand.isFinished();
-		       }else{
-		        return driveCommand.isFinished();
-		       }
-	}
+    // Called once after isFinished returns true
+    protected void end() {
+        if (Robot.cvChooser.getSelected()) {
+            cvCommand.end();
+        } else {
+            driveCommand.end();
+        }
+    }
 
-	// Called once after isFinished returns true
-	protected void end() {
-	    if (Robot.cvChooser.getSelected()){
-	        cvCommand.end();
-	    }else
-	        driveCommand.end();
-	    }
-	
-
-	// Called when another command which requires one or more of the same
-	// subsystems is scheduled to run
-	protected void interrupted() {
-	    if(Robot.cvChooser.getSelected()){
-	        cvCommand.interrupted();
-	    }else{
-	        driveCommand.interrupted();
-	    }
-	}
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+        if (Robot.cvChooser.getSelected()) {
+            cvCommand.interrupted();
+        } else {
+            driveCommand.interrupted();
+        }
+    }
 }
