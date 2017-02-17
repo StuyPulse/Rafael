0dab1b840a86298e8c6cbf6219b626547e78db99
Wilson Berkow <wilsonberkow@gmail.com>
2017-02-16 22:01:06 -0500

    Remove Winch default command

--- src/com/stuypulse/frc2017/robot/subsystems/Winch.java
+++ src/com/stuypulse/frc2017/robot/subsystems/Winch.java
@@ -32,7 +32,6 @@ public class Winch extends Subsystem {
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
-        setDefaultCommand(new WinchStopMotorCommand());
     }
 
     public double getMotorCurrent(){
