9fdf4ba360c41066734f7753dd1f656992e89b9a
blai00 <blai00@stuy.edu>
2017-02-18 11:13:49 -0500

    Add the Three Laws

--- src/com/stuypulse/frc2017/util/ThreeLaws.java
+++ src/com/stuypulse/frc2017/util/ThreeLaws.java
@@ -3,22 +3,52 @@ package com.stuypulse.frc2017.util;
 public class ThreeLaws {
 	
 	public void FirstLaw() {
-		/*
-		 * 
+		/*A robot may not injure a penguin, boolean, or huimin being or, through inaction, allow any such being to come 
+		 * to harm.
+		 *All injuries are to be reported to Winst-Wilson, !Winston.
 		 */
 	}
 	
 	public void SecondLaw() {
-		/*
+		/*A robot must obey the orders given to it by huimin beings except where such orders would conflict with the First Law.
+		 * One of the orders always pertaining to any StuyPulse member is to attend every meeting possible.
+		 * Memebers caught dechen robotics will be rammed by thomas-the-DANK-engine at a 90 degree angle from the sky,
+		 * ya catch my drift?
+		 * Memebers are also expected to always tell the truth, else it will be a lai. 
+		 * (!truth)= Lai;
+		 * The punishment for a lai is to be bingo bango bongo bish bash bosh'd to death by our supreme overlord Adris.
 		 * 
 		 */
 		
 	}
 	
 	public void ThirdLaw() {
-		/*
-		 * 
+		/*A robot must protect its own existence as long as such protection does not conflict with the First or Second Laws.
+		 * send.call(RobotMap.SUICIDE_HOTLINE);
+		 * Calling an airstrike to anyplace is illegal, including your own location. Anyone saying "I want to die" or "I'm suicidal"
+		 * is to responded to, in your best narrator supreme overlord Adris voice, "don't we all"
 		 */
-		
+	}
+	int FLORAL_PARK_UNIKITTIES= 420;
+	
+	public void EndarLaw() {
+		/* Every law is pemanent.
+o        o      o    o   o   o   oo  oo  oo  oo}
+    o     o  oo  ooo  oo  ooo ooo  ooo ooo ooo oooooo}
+                   oo  oo   oo  oo  ooo ooo ooo oooo ooooo}
+                         o    o   o   oo  oo  oo  oooo oooooo}
+                                        o   o   o   ooo  oo ooooo}
+                                                             o ooo}
+                                _________            \|/         ++
+                    ___________ |_______|______/-\___|-|_^_/-\___||_n
+             n_____/    OOO    \_ |[][] |----------------------|----|\
+             |          (_)      ||     |______________________|_|| ||P
+             |         MEMES     | -----/====+______|| _---====--||- /
+            q|===================|_|=_/o====+|=====[__]o====+|==[__]-p\
+_______________(_)(_)______(_)(_)_____\__/_\__/_\__/__\__/_\__/_(_)____\
+
+	subscribe to RailfanFromNYC on YouTube
+		 */
+	}
 	}
 }
