28934ced933d1fd6cc7873640c3f04a30da176cb
adrisj7 <adrisj7@gmail.com>
2017-02-20 09:15:29 -0500

    Fix ScoreGearCommand to pull back gear pusher before opening trap

--- src/com/stuypulse/frc2017/robot/commands/ScoreGearCommand.java
+++ src/com/stuypulse/frc2017/robot/commands/ScoreGearCommand.java
@@ -1,6 +1,6 @@
 package com.stuypulse.frc2017.robot.commands;
 
-import com.stuypulse.frc2017.robot.commands.auton.DoubleSequentialCommand;
+import com.stuypulse.frc2017.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
@@ -10,11 +10,15 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class ScoreGearCommand extends CommandGroup {
 
     public ScoreGearCommand(double delayinseconds) {
-    	addSequential(new GearPusherPushGearCommand());
+    	// Before scoring: RETRACT pusher, close trap, wait for the geartrap to open, push
+    	addSequential(new GearPusherRetractGearCommand());
     	addSequential(new GearTrapReleaseGearCommand());
+    	addSequential(new DelaySecondsCommand(RobotMap.GEAR_PUSHER_PUSH_AFTER_GEAR_OPEN_TIME));
+    	addSequential(new GearPusherPushGearCommand());
     	addSequential(new DelaySecondsCommand(delayinseconds));
     	addSequential(new GearPusherRetractGearCommand());
-        // To run multiple commands at the same time,
+
+    	// To run multiple commands at the same time,
         // use addParallel()
         // e.g. addParallel(new Command1());
         //      addSequential(new Command2());
