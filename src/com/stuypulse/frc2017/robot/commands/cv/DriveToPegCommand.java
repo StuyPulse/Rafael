d32b23d1c8cd6d066b8d96d185df71a5697411f8
Wilson Berkow <wilsonberkow@gmail.com>
2017-03-03 09:24:31 -0500

    Save CV image used for alignments

--- src/com/stuypulse/frc2017/robot/commands/cv/DriveToPegCommand.java
+++ src/com/stuypulse/frc2017/robot/commands/cv/DriveToPegCommand.java
@@ -17,7 +17,7 @@ public class DriveToPegCommand extends EncoderDrivingCommand {
 
     @Override
     protected double getInchesToMove() {
-        Vector cvReading = Robot.liftVision.mTip_processImage(false);
+        Vector cvReading = Robot.liftVision.mTip_processImage(true);
         boolean foundGoal = cvReading != null;
         Robot.cvFoundGoal = foundGoal;
         if (foundGoal) {
--- src/com/stuypulse/frc2017/robot/commands/cv/RotateToLiftCommand.java
+++ src/com/stuypulse/frc2017/robot/commands/cv/RotateToLiftCommand.java
@@ -23,7 +23,7 @@ public class RotateToLiftCommand extends GyroRotationalCommand {
 
     @Override
     protected double getDesiredAngle() {
-        Vector cvReading = Robot.liftVision.mTip_processImage(false);
+        Vector cvReading = Robot.liftVision.mTip_processImage(true);
         boolean foundGoal = cvReading != null;
         Robot.cvFoundGoal = foundGoal;
         if (foundGoal) {
