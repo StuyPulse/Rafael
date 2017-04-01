fed2d45d807feed35b2f1143fd8ff7c5f65bbac8
Ryan Kim <ryekimmi@gmail.com>
2017-04-01 18:14:37 -0400

    Update pressure sensor code

---
+++ src/com/stuypulse/frc2017/util/PressureSensor.java
@@ -0,0 +1,35 @@
+package com.stuypulse.frc2017.util;
+
+import com.stuypulse.frc2017.robot.Robot;
+import com.stuypulse.frc2017.robot.RobotMap;
+
+public class PressureSensor {
+
+    private AnalogInput pressureSensor;
+
+    public PressureSensor() {
+        pressureSensor = new AnalogInput(RobotMap.PRESSURE_SENSOR_PORT);
+    }
+    
+    int Vcc = 5;
+    
+    public double getVout() {
+        return pressureSensor.getVoltage();
+    }
+    
+    public double getPressure() {
+        return ((250 * (getVout() / Vcc)) - 25);
+    }
+    
+    public boolean isPressureLow() {
+        return getPressure() < RobotMap.PRESSURE_SENSOR_THRESHOLD;
+    }
+    
+    public void pressureLEDSignalControl() {
+        if (isPressureLow()) {
+            Robot.ledGearSensingSignal.stayOn();
+        } else {
+            Robot.ledGearSensingSignal.stayOff();
+        }
+    }
+}
