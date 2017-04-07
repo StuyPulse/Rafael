a860dd6f35dbea587835fa00df6b498da47b64ac
Prog694 <prog694@gmail.com>
2017-04-07 08:56:47 -0400

    Fix DrivetrainStop command to end when not held

--- src/com/stuypulse/frc2017/robot/commands/DrivetrainStopCommand.java
+++ src/com/stuypulse/frc2017/robot/commands/DrivetrainStopCommand.java
@@ -26,7 +26,7 @@ public class DrivetrainStopCommand extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
