c45d53ed111b4df33d4651d9a78d43034c447896
Prog694 <prog694@gmail.com>
2017-04-08 11:44:45 -0400

    Make boiler-gear auton try to score and correct auton selector labels

--- src/com/stuypulse/frc2017/robot/commands/auton/ScoreBoilerGearCommand.java
+++ src/com/stuypulse/frc2017/robot/commands/auton/ScoreBoilerGearCommand.java
@@ -2,6 +2,7 @@ package com.stuypulse.frc2017.robot.commands.auton;
 
 import com.stuypulse.frc2017.robot.Robot;
 import com.stuypulse.frc2017.robot.commands.DriveInchesEncodersCommand;
+import com.stuypulse.frc2017.robot.commands.DriveInchesPIDCommand;
 import com.stuypulse.frc2017.robot.commands.GearTrapTrapGearCommand;
 import com.stuypulse.frc2017.robot.commands.RotateDegreesGyroCommand;
 import com.stuypulse.frc2017.robot.commands.ScoreGearCommand;
@@ -34,8 +35,7 @@ public class ScoreBoilerGearCommand extends CommandGroup {
         if (useCV) {
             addSequential(new SetupForGearCommand());
         } else {
-            return;
-            //addSequential(new DriveInchesEncodersCommand(AFTER_TURN_TO_BOILER_GEAR_DISTANCE));
+            addSequential(new DriveInchesPIDCommand(0.5, AFTER_TURN_TO_BOILER_GEAR_DISTANCE));
         }
         addSequential(new ScoreGearCommand());
         addSequential(new DriveInchesEncodersCommand(BOILER_GEAR_REVERSE_DISTANCE));
