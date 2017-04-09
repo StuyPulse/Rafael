6193a447f9ec48517e021d605020fb199eb95eef
Prog694 <prog694@gmail.com>
2017-04-09 10:28:34 -0400

    Reduce side-gear auton final distances to 24 inches
    They were sky-high (40in and 80in) from testing,
    when it didn't move at all.

--- src/com/stuypulse/frc2017/robot/commands/auton/ScoreBoilerGearCommand.java
+++ src/com/stuypulse/frc2017/robot/commands/auton/ScoreBoilerGearCommand.java
@@ -15,9 +15,9 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  *
  */
 public class ScoreBoilerGearCommand extends CommandGroup {
-    public static final double START_TO_BOILER_GEAR_TURN_DISTANCE = 100.0;//114.3;
+    public static final double START_TO_BOILER_GEAR_TURN_DISTANCE = 100.0;
     public static final double BOILER_GEAR_TURN_TO_BOILER_GEAR_ANGLE = -60;
-    public static final double AFTER_TURN_TO_BOILER_GEAR_DISTANCE = 80;
+    public static final double AFTER_TURN_TO_BOILER_GEAR_DISTANCE = 24;
     public static final double BOILER_GEAR_REVERSE_DISTANCE = -12;
 
     public ScoreBoilerGearCommand(boolean score) {
